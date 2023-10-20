771676b0c7e365252e374c774f0b876645af8873
Vahidone <v.ban.roma@gmail.com>
2023-10-20 18:25:43 +0200

    create cards for section-company

---
+++ src/components/partials/Cards-company.vue
@@ -0,0 +1,55 @@
+
+
+<script>
+export default {
+  name: 'Cards-company'
+}
+</script>
+
+<template>
+  <div class="container-csm mt-5">
+    <div class="row">
+      <div class="col-6">
+        <div class="card">
+          <h2><i class="fa-solid fa-lock"></i>  Security</h2>
+          <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div class="card">
+          <h2><i class="fa-solid fa-lock"></i>  Security</h2>
+          <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div class="card">
+          <h2><i class="fa-solid fa-lock"></i>  Security</h2>
+          <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit.</p>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div class="card">
+          <h2><i class="fa-solid fa-lock"></i>  Security</h2>
+          <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+
+.card {
+  background-color: $bg-section-company;
+  color: white;
+  margin-top: 30px;
+  
+}
+
+
+</style>
