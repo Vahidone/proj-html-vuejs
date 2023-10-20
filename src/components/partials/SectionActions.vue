df06bf8ab174d4f9a190a6a421b3ae4d2c63ce92
Vahidone <v.ban.roma@gmail.com>
2023-10-20 18:29:27 +0200

    feat: create section-actions

---
+++ src/components/partials/SectionActions.vue
@@ -0,0 +1,26 @@
+
+
+<script>
+export default {
+  name: 'SectionActions'
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="top">
+
+    </div>
+
+    <div class="down"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+.top {
+  text-align: center;
+}
+
+</style>
