30e305f559a7ece39901849940fe6afc74ec6f0d
Vahidone <v.ban.roma@gmail.com>
2023-10-20 16:15:57 +0200

    feat: add main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,24 @@
 <script>
+import SectionCompany from './partials/SectionCompany.vue';
+import SectionExcellence from './partials/SectionExcellence.vue';
+import SectionReady from './partials/SectionReady.vue';
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    SectionReady,
+    SectionExcellence,
+    SectionCompany
+  
+  }
 }
 </script>
 
 <template>
-  <h2>Main</h2>
+  <main>
+    <SectionReady />
+    <SectionExcellence />
+    <SectionCompany />
+  </main>
 </template>
 
 <style lang="scss">
