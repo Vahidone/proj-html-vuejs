9a59d3f35283384267fcbd4fd6639fe1bd7641ef
Vahidone <v.ban.roma@gmail.com>
2023-10-20 18:25:19 +0200

    feat: create cards container for section-company

---
+++ src/components/partials/CardsContainer-company.vue
@@ -0,0 +1,21 @@
+
+
+<script>
+import CardsCompany from './Cards-company.vue';
+export default {
+  name: 'CardsContainer-company',
+  components: {
+    CardsCompany
+  }
+}
+</script>
+
+<template>
+  <CardsCompany />
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+
+</style>
