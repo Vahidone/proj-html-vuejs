c10a98daaea14717b0cc5ad46b8461fc6ba451d6
Vahidone <v.ban.roma@gmail.com>
2023-10-21 09:52:09 +0200

    feat:add cardsContaineer component for section-action

---
+++ src/components/partials/CardsContainer-actions.vue
@@ -0,0 +1,22 @@
+
+<script>
+import CardsActions from './Cards-actions.vue';
+export default {
+  name: 'CardsContainer-actions',
+  components: {
+    CardsActions
+  }
+
+}
+</script>
+
+<template>
+  <CardsActions />
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+
+
+</style>
