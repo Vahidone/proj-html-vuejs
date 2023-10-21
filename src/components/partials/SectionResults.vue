c158a6559fd0aba144e0abb76181a4fb74cc5524
Vahidone <v.ban.roma@gmail.com>
2023-10-21 10:08:10 +0200

    feat: create Section-Results component

---
+++ src/components/partials/SectionResults.vue
@@ -0,0 +1,23 @@
+
+
+<script>
+export default {
+  name: 'SectionResults'
+}
+</script>
+
+<template>
+  <div class="container">
+    <section>
+
+    </section>
+
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+
+</style>
