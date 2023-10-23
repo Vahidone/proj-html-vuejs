22439873e9071eefb8b96e3881b3716743ac5110
Vahidone <v.ban.roma@gmail.com>
2023-10-23 18:30:23 +0200

    feat: create list component for Actions section

---
+++ src/components/partials/ListACtions.vue
@@ -0,0 +1,72 @@
+
+<script>
+export default {
+  name: 'ListActions',
+
+  data () {
+    return {
+      ListActions: [
+        {
+          content: "ALL"
+        },
+        {
+          content: "INSTITUTIONAL"
+        },
+        {
+          content: "SOCIAL"
+        },
+        {
+          content: "EVENTS"
+        },
+        {
+          content: "RESULTS"
+        },
+        {
+          content: "INNOVATION"
+        },
+        {
+          content: "ENVIRONMENT"
+        },
+        {
+          content: "TECHNOLOGY"
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="(list, index) in ListActions" :key="index"><a href="#">{{ list.content }}</a></li>
+  </ul>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../../scss/main.scss" as *;
+@use "../../scss/partials/variables" as *;
+
+ul {
+  li {
+    margin-top: 70px;
+    display: inline-block;
+    margin-right: 20px;
+    a {
+      color: gray;
+      &:hover {
+        color: $btn-green;
+        background-color: #D9EBED;
+        padding: 7px;
+        cursor: pointer;
+      }
+        
+    }
+      
+  }
+
+}
+
+
+</style>
