87b9f2fe5b217445251f675fb299c22c8f49f163
Vahidone <v.ban.roma@gmail.com>
2023-10-23 21:49:05 +0200

    add: add lists array of footer  to store.js

---
+++ src/data/store.js
@@ -0,0 +1,136 @@
+import 'typeface-roboto';
+
+export default {
+  headerContactsList: [
+
+    {
+      href: '#',
+      content: "+1 (305) 1234-5678",
+      icon: "fa-solid fa-phone"
+    },
+    {
+      href: '#',
+      content: "hello@example.com",
+      icon: "fa-solid fa-envelope"
+    },
+    {
+      href: '#',
+      content: " ",
+      icon: "fa-brands fa-facebook-f"
+    },
+    {
+      href: '#',
+      content: " ",
+      icon: "fa-brands fa-twitter"
+    },
+    {
+      href: '#',
+      content: " ",
+      icon: "fa-brands fa-linkedin-in"
+    }
+
+  ],
+
+
+
+  footerListaUno: [
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "The Company"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Institutional"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Social & Events"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Innovation"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Environment"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Technology"
+    },
+  ],
+
+  footerListaDue: [
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Audit & Assurance"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Finantial Advisory"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Analytics M&A"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Middle Marketing"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Legal Consulting"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Regulatory Risk"
+    },
+  ],
+
+  footerListaTre: [
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Responsability"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Terms of Use"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "About Cookies"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Privacy Policy"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content: "Accessibility"
+    },
+    {
+      icon: "fa-solid fa-chevron-right",
+      href: "#",
+      content:  "Information"
+    },
+  ],
+
+
+}
